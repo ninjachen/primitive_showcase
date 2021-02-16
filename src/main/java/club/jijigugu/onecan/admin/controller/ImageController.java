7598456cb3d46b3f78f2d9385f690faa90e3b7d9
Ninja <jiachenning@gmail.com>
2021-02-16 16:12:36 +0800

    Add demo controller

--- src/main/java/club/jijigugu/onecan/admin/controller/ImageController.java
+++ src/main/java/club/jijigugu/onecan/admin/controller/ImageController.java
@@ -27,31 +27,38 @@ public class ImageController {
     @PostMapping("/primitive")
     public HashMap primitive(@RequestParam("file") MultipartFile file,
                          @RequestParam(value = "n", defaultValue = "100") int n,
-                         @RequestParam(value = "m", defaultValue = "7") int m) throws IOException {
-        File dir = new File("/tmp/ninja/");
-        if (!dir.exists())
-            dir.mkdirs();
-
-        String fileName = file.getOriginalFilename();
-        if (fileName == null || fileName.isEmpty()) {
-            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            fileName = sdf.format(new Date());
-        }
-        File sourceFile = new File(dir, fileName);
-        file.transferTo(sourceFile);
-
-        // primitive -i IMG_20181026_115438.jpg -o IMG_20181026_115438_deal_3.jpg -n 200 -m 8
-        long l = System.currentTimeMillis();
-        String input = sourceFile.getAbsolutePath();
-        File newFile = new File(dir, l + ".png");
-        String output = newFile.getAbsolutePath();
-        String cmd = String.format("primitive -i %s -o %s -n %d -m %d", input, output, n, m);
-        exec(cmd);
-        String url = buildImageUrl(newFile.getName());
+                         @RequestParam(value = "m", defaultValue = "7") int m) throws IOException, InterruptedException {
+        Thread.sleep(1000);
         HashMap<String, String> resp = new HashMap<>();
-        resp.put("url", url);
+        resp.put("url", "https://api.meaningless.cn/image/view/my_picture_after");
         resp.put("msg", "n is number. \n m is type. \n 0=combo 1=triangle 2=rect 3=ellipse 4=circle 5=rotatedrect 6=beziers 7=rotatedellipse 8=polygon (default 1)");
         return resp;
+//
+//
+//        File dir = new File("/tmp/ninja/");
+//        if (!dir.exists())
+//            dir.mkdirs();
+//
+//        String fileName = file.getOriginalFilename();
+//        if (fileName == null || fileName.isEmpty()) {
+//            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//            fileName = sdf.format(new Date());
+//        }
+//        File sourceFile = new File(dir, fileName);
+//        file.transferTo(sourceFile);
+//
+//        // primitive -i IMG_20181026_115438.jpg -o IMG_20181026_115438_deal_3.jpg -n 200 -m 8
+//        long l = System.currentTimeMillis();
+//        String input = sourceFile.getAbsolutePath();
+//        File newFile = new File(dir, l + ".png");
+//        String output = newFile.getAbsolutePath();
+//        String cmd = String.format("primitive -i %s -o %s -n %d -m %d", input, output, n, m);
+//        exec(cmd);
+//        String url = buildImageUrl(newFile.getName());
+//        HashMap<String, String> resp = new HashMap<>();
+//        resp.put("url", url);
+//        resp.put("msg", "n is number. \n m is type. \n 0=combo 1=triangle 2=rect 3=ellipse 4=circle 5=rotatedrect 6=beziers 7=rotatedellipse 8=polygon (default 1)");
+//        return resp;
     }
 
     @GetMapping("/view/{filename}")
